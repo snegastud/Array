9bccbc0b1b5319383bea9cb1f4654ca8e104635d
snegastud <129963809+snegastud@users.noreply.github.com>
2025-05-22 22:12:50 +0530

    Add files via upload

---
+++ ArrayMatrix.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ArrayMatrix{
+public static void main(String args[]){
+int[][] arr={{2,3,4},               //arr[0]={2,3,4}
+{4,5,6},
+{7,8,9}};
+for(int i=0;i<3;i++){
+for(int j=0;j<3;j++){
+System.out.print(arr[i][j]+" ");
+
+}
+System.out.println();
+}
+}
+}
